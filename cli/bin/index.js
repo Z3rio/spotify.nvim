5edc151c26e1f0ce9ea1ae2c8ddbbc8079f2d15b
Zerio <neoboij@gmail.com>
2024-01-20 01:11:37 +0100

    chore: setup simple auth handling

--- cli/bin/index.js
+++ cli/bin/index.js
@@ -1,2 +1,42 @@
 #! /usr/bin/env node
-console.log("hello?");
+import open from "open";
+import Koa from "koa";
+import Router from "@koa/router";
+
+const args = process.argv.splice(2);
+
+if (args[0]) {
+  switch (args[0]) {
+    case "auth":
+      const clientId = args[1];
+      const redirectUri = "http://localhost:8888/callback";
+
+      if (clientId && redirectUri) {
+        const app = new Koa();
+        const router = new Router();
+
+        router.get("", "/callback", (ctx) => {
+          const { code } = ctx.request.query;
+
+          if (code && typeof code == "string") {
+            console.log(code);
+            process.exit(0);
+          }
+        });
+
+        app.use(router.routes()).use(router.allowedMethods());
+
+        // app.on("listening", () => {
+
+        // });
+
+        app.listen(8888);
+
+        open(
+          `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}`
+        );
+      }
+
+      break;
+  }
+}
--- lua/spotify/init.lua
+++ lua/spotify/init.lua
@@ -7,9 +7,6 @@ M.Opts = {
   redirectUri = "http://localhost:8888/callback",
 }
 
-M.IsAuthorized = false
-M.IsServerStarted = false
-
 ---@class SpotifyOption
 ---@field debug boolean
 ---@field redirectUri string
@@ -35,48 +32,17 @@ M.execCommand = function(cmd)
   return nil
 end
 
+M.auth = function()
+  print(M.execCommand("spotifynvimcli auth " .. M.Opts.clientId))
+end
+
 ---@param options SpotifyOption
 M.setup = function(options)
   M.debugLog("called setup")
 
   M.Opts = vim.tbl_deep_extend("force", M.Opts, options)
 
-  if M.IsServerStarted == false then
-    M.StartServer()
-  end
-
-  if M.IsAuthorized == false then
-    M.authorize()
-  end
-end
-
-M.OpenBrowserLink = function(link)
-  M.debugLog("tried to open link: " .. link)
-
-  if vim.fn.has("mac") == 1 then
-    vim.cmd("silent !open " .. link)
-  elseif vim.fn.has("unix") == 1 then
-    vim.cmd("silent !xdg-open " .. link)
-  else
-    vim.cmd('silent !start "' .. link .. '"')
-  end
-end
-
-M.StartServer = function()
-  M.IsServerStarted = true
-  local resp = M.execCommand("spotifynvimcli")
-  print("resp", resp)
-end
-
-M.authorize = function()
-  M.debugLog("tried to authorize")
-
-  -- M.OpenBrowserLink(
-  --   "https://accounts.spotify.com/authorize?client_id="
-  --   .. M.Opts.clientId
-  --   .. "&response_type=code&redirect_uri="
-  --   .. M.Opts.redirectUri
-  -- )
+  M.auth()
 end
 
 return M
